590d379065da29e31ba411ccd4492ad1a16b9aba
bragalund <bakkelund_91@hotmail.com>
2019-12-23 22:01:51 +0100

    Spider is working and storing numbers as float in json-file

--- vitual_python_env/nordnet2/nordnet2/spiders/nordnetspider2.py
+++ vitual_python_env/nordnet2/nordnet2/spiders/nordnetspider2.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
+import datetime
 import scrapy
+
+
+def convertStringToFloat(someString):
+    return float(someString.replace(",","."))
 
 
 class Nordnetspider2Spider(scrapy.Spider):
@@ -9,7 +14,27 @@ class Nordnetspider2Spider(scrapy.Spider):
                   'https://www.nordnet.no/market/stocks?sortField=diff_pct&sortOrder=desc&page=3&exchangeCountry=NO']
 
     def parse(self, response):
-        for stockline in response.xpath("//tbody/tr/td[@data-title='Navn']/a"):
+        for stockline in response.xpath("//tbody/tr"):
+
+            dateToday = datetime.datetime.today()
+            stockname = stockline.xpath("td[@data-title='Navn']/a/text()").get()
+            lastPriceToday = stockline.xpath("td[@data-title='Siste']/span/span[@aria-hidden='true']/text()").get()
+            lastPriceToday = convertStringToFloat(lastPriceToday)
+            highestPriceToday = stockline.xpath("td[@data-title='Høy']/text()").get()
+            highestPriceToday = convertStringToFloat(highestPriceToday)
+            lowestPriceToday = stockline.xpath("td[@data-title='Lav']/text()").get()
+            lowestPriceToday = convertStringToFloat(lowestPriceToday)
+            turnover = stockline.xpath("td[@data-title='Omsetning']/span/span[@aria-hidden='true']/text()").get()
+            turnover = convertStringToFloat(turnover)
+            timeOfDay = stockline.xpath("td[@data-title='Tid']/span/text()").get()
+
             yield{
-                'stockname': stockline.xpath("text()").get()
+                'date': dateToday,
+                'stockname': stockname,
+                'last_price_today': lastPriceToday,
+                'highest_price_today': highestPriceToday,
+                'lowest_price_today': lowestPriceToday,
+                'turnover': turnover,
+                'time_of_day': timeOfDay
             }
+
