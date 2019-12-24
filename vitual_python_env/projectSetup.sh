a3595cd5e7fd4f3e2eec51aa99584f40b4100641
bragalund <bakkelund_91@hotmail.com>
2019-12-24 04:03:26 +0100

    Deleted old misspelled virtualenv

--- vitual_python_env/projectSetup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-pip3 install virtualenv
-virtualenv myNewVirtualenv                  # create new virtual environment for dependencies to be stored in
-cd myNewVirtualenv
-source bin/activate                         # activate virtual environment
-pip3 install scrapy                         # install scrapy(webcrawler-framework) within virtual environment
-scrapy startproject aftenposteentestproject # create scrapy-project structure
